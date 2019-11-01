4a3f675f2e74f331cab45e44e6d635e0f0851ce4
mika_go <mihail.kochetkov@icloud.com>
2019-11-01 21:36:28 +0300

    init

---
+++ build/precache-manifest.a77acf1fc5954f95475c23c6db828e56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "787bb2504624139045e7fe29ec7098ce",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d5a7b6a9227e05e7b08c",
+    "url": "./static/css/main.dfca195d.chunk.css"
+  },
+  {
+    "revision": "a84489e259f6bb749cc1",
+    "url": "./static/js/2.a0ebcd3a.chunk.js"
+  },
+  {
+    "revision": "d5a7b6a9227e05e7b08c",
+    "url": "./static/js/main.ca639907.chunk.js"
+  },
+  {
+    "revision": "cdc0a79acbd9b0b61825",
+    "url": "./static/js/runtime-main.e69bcd59.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "./static/media/logo.25bf045c.svg"
+  }
+]);
