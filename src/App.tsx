013e4e7673dcbba137d471a3fa2fb7b44e3096ea
mika_go <mihail.kochetkov@icloud.com>
2019-11-01 22:51:44 +0300

    Add basic structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import React, {Component} from "react";
+import {Container, Navbar, NavbarBrand} from "react-bootstrap";
+import "./App.sass";
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className={"app"}>
+        <Navbar bg={"dark"} variant={"dark"} expand={"lg"}>
+          <NavbarBrand href={"/networks/index.html"}>Сети и телекомуникации</NavbarBrand>
+        </Navbar>
 
+        <Container as={"main"}>
+
+        </Container>
+        <Container fluid className={"text-center bg-dark text-light"} as={"footer"}>
+          <Container className={"py-3"}>
+            <small>Copyright &copy; Михаил Кочетков ИУ5-51</small>
+          </Container>
+        </Container>
       </div>
     );
   }
--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.sass';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import "bootstrap/dist/css/bootstrap.min.css";
+import {Provider} from "mobx-react";
+import RootStore from "./store";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={new RootStore()}>
+    <App/>
+  </Provider>, document.getElementById('root'));
 
 serviceWorker.unregister();
